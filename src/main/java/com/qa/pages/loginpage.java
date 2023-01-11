581f750943a5513a99f10a2e2d653992c8289210
ajay gawade <ajaygawade1111@gmail.com>
2023-01-11 16:23:51 +0530

    searchpage and loginpage changes

--- src/main/java/com/qa/pages/loginpage.java
+++ src/main/java/com/qa/pages/loginpage.java
@@ -4,6 +4,12 @@ public class loginpage {
 
 	public void loginpage()
 	{
-		System.out.println("login page");
+		System.out.println("login page ");
+		System.out.println("login page -> change 1");
+	}
+	public void signup()
+	{
+		System.out.println("sinup page ");
+		
 	}
 }
